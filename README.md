8486cb3692d1173e5e514ddc27453968399193d6
danielemantovani <daniele.mantovani124@gmail.com>
2024-03-28 18:58:55 +0100

    feat: mail

--- README.md
+++ README.md
@@ -6,8 +6,17 @@ stampa un messaggio appropriato sull’esito del controllo.
 
 ### Soluzione
 
+- INPUT
 
 1. Chiedere all'utente tramite "prompt" il suo indirizzo mail
-2. Creare una lista di persone autorizzate ad accedere alla casella di posta
+
+- ESECUZIONE LOGICA
+
+2. Creare una lista di email autorizzate ad accedere alla casella di posta
 3. Controllare se la mail inserita dall'utente è tra le mail presenti in lista
+    se la mail è presente nella lista stampa "accesso consentito"
+    altrimenti stampa "accesso non consentito"
+
+- OUTPUT 
+
 4. Stampare l'esito in console e in pagina
--- mail/script.js
+++ mail/script.js
@@ -1,6 +1,29 @@
 // test
 // alert ("mail")
 
+const emailAdress = ["daniele94@gmail.com", "danielemanto@virgilio.it", "dani1994@live.it", "paperella@libero.it"]; //arrray
+
 //chiedo all'utente di digitare il suo indirizzo mail
-prompt ("Digita il tuo indirizzo mail")
+const mail = prompt ("Digita il tuo indirizzo mail")
+console.warn (mail)
+
+let found = false;
+
+for (let i = 0; i < emailAdress.length; i++) {
+    console.log( i, emailAdress[i]);
+    if (mail === emailAdress[i]) {
+        console.warn ("accesso");
+        found = true;
+        break;
+    }
+}
+
+if (found === true){
+    console.warn ("ACCESSO CONSENTITO");
+document.getElementById ("result").innerHTML = "Accesso consentito";
+}else {
+    console.warn ("ACCESSO NEGATO");
+    document.getElementById ("result").innerHTML = "Accesso negato";
+}
+
 
