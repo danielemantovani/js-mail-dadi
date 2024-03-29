b5f9b8cd7415eb405d6a3515b7cd8f48e2dbcc15
danielemantovani <daniele.mantovani124@gmail.com>
2024-03-29 20:11:57 +0100

    feat: esercizio dadi

--- dadi/script.js
+++ dadi/script.js
@@ -1,2 +1,42 @@
 // test
-alert ("dadi")
+// alert ("dadi")
+
+const lanciaBtnElem = document.getElementById ("lancia");
+console.log ("lanciaBtnElem")
+
+const numPlayer =Math.floor(Math.random() *6) + 1;
+console.log (numPlayer);
+
+const numComputer = Math.floor(Math.random() * 6) + 1;
+console.log (numComputer);
+
+
+
+if (numPlayer > numComputer) {
+    console.log("Il giocatore vince");
+    result = "Il giocatore vince";
+} else if (numComputer > numPlayer) {
+    console.log("Il banco vince");
+    result = "Il banco vince";
+} else if (numComputer === numPlayer)  {
+    console.log("Pareggio");
+    result = "Pareggio";
+}
+
+
+
+
+// OUTPUT
+
+document.querySelector (".player").innerHTML = numPlayer;
+
+document.querySelector (".computer").innerHTML = numComputer;
+
+document.getElementById ("result").innerHTML = result;
+
+
+
+
+
+
+
