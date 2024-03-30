2a8e1ad0d9c0314adabc59862c4eb80a7fc68e0c
danielemantovani <daniele.mantovani124@gmail.com>
2024-03-30 23:07:02 +0100

    fix: mail

--- mail/script.js
+++ mail/script.js
@@ -1,29 +1,45 @@
 // test
 // alert ("mail")
 
+"use strict";
+
 const emailAdress = ["daniele94@gmail.com", "danielemanto@virgilio.it", "dani1994@live.it", "paperella@libero.it"]; //arrray
 
-//chiedo all'utente di digitare il suo indirizzo mail
-const mail = prompt ("Digita il tuo indirizzo mail")
-console.warn (mail)
-
+//creo variabile d'appoggio
 let found = false;
 
-for (let i = 0; i < emailAdress.length; i++) {
-    console.log( i, emailAdress[i]);
-    if (mail === emailAdress[i]) {
-        console.warn ("accesso");
-        found = true;
-        break;
-    }
-}
+//trovo bottone in pagina
+const checkBtn = document.getElementById("check-button");
+console.log(checkBtn);
 
-if (found === true){
-    console.warn ("ACCESSO CONSENTITO");
-document.getElementById ("result").innerHTML = "Accesso consentito";
-}else {
-    console.warn ("ACCESSO NEGATO");
-    document.getElementById ("result").innerHTML = "Accesso negato";
-}
+// collego input
+const mail = document.getElementById("check-button")
+
+//creo evento al click del bottone conferma
+checkBtn.addEventListener("click", function () {
+
+    const mail= document.getElementById("indirizzo-mail").value;
+
+
+    for (let i = 0; i < emailAdress.length; i++) {
+        console.log(i, emailAdress[i]);
+        if (mail === emailAdress[i]) {
+            console.warn("Mail trovata");
+            found = true;
+            break;
+        }
+    }
+
+    if (found === true) {
+        console.warn("ACCESSO CONSENTITO");
+        document.getElementById("result").innerHTML = "Accesso consentito";
+    } else {
+        console.warn("ACCESSO NEGATO");
+        document.getElementById("result").innerHTML = "Accesso negato";
+    }
+
+})
+
+
 
 
