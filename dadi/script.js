4d0476d5496b17b10330fc376633538e66f263bb
danielemantovani <daniele.mantovani124@gmail.com>
2024-03-30 13:27:31 +0100

    feat: commenti

--- dadi/script.js
+++ dadi/script.js
@@ -1,19 +1,23 @@
 // test
 // alert ("dadi")
 
+//trovo il bottone in pagina
 const lanciaBtnElem = document.getElementById ("lancia");
 console.log (lanciaBtnElem);
 
+// aggiungo l'evento al click del bottone 
 lanciaBtnElem.addEventListener("click", function () {
 
-
+//variabile per generare nr casuale per il player
 const numPlayer =Math.floor(Math.random() *6) + 1;
 console.log (numPlayer);
 
+//variabile per generare nr casuale per il computer
 const numComputer = Math.floor(Math.random() * 6) + 1;
 console.log (numComputer);
 
 
+//esecuzione logica
 
 if (numPlayer > numComputer) {
     console.log("Il giocatore vince");
@@ -27,9 +31,7 @@ if (numPlayer > numComputer) {
 }
 
 
-
-
-// OUTPUT
+// STAMPA IN PAGINA
 
 document.querySelector (".player").innerHTML = numPlayer;
 
